58c25579fef6e0471229217ec03dad4355f2fc23
mfwolffe <wolffemf@dukes.jmu.edu>
2025-07-04 21:05:28 -0400

    fix the 404?

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  output: 'standalone',
   // Disable SSR for Leaflet components
   transpilePackages: ['leaflet'],
   
